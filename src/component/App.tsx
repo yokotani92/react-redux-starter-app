bce1cb4962ea450563de652449cc1aa313bced96
Yuki Yokotani <yokotani.yuki@gmail.com>
2020-04-10 18:25:25 +0900

    Import Material UI

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,9 +1,28 @@
 import * as React from 'react';
 import Counter from './Counter';
+import { CssBaseline, createMuiTheme, ThemeProvider, useMediaQuery } from "@material-ui/core";
+import { lightBlue, pink } from '@material-ui/core/colors';
 
 const App: React.FC = () => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = createMuiTheme({
+    palette: {
+      type: prefersDarkMode ? 'dark' : 'light',
+      primary: {
+        main: lightBlue.A400,
+      },
+      secondary: {
+        main: pink.A200,
+      },
+    },
+  });
+
   return (
-    <Counter />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Counter />
+    </ThemeProvider>
   );
 };
 
--- src/component/Counter.tsx
+++ src/component/Counter.tsx
@@ -2,17 +2,33 @@ import * as React from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { increment, decrement } from './../redux/actions';
 import { RootState } from './../redux/reducers/index';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            textAlign: 'center',
+        },
+        button: {
+            '& > *': {
+                margin: theme.spacing(1),
+            },
+        },
+    }),
+);
 
 const Counter: React.FC = () => {
     const count = useSelector<RootState, number>(state => state.counter.count);
     const dispatch = useDispatch();
+    const classes = useStyles();
 
     return (
-        <div>
+        <div className={classes.root}>
             <h1>{count}</h1>
-            <div>
-                <button onClick={() => dispatch(increment())} > INCREMENT</button>
-                <button onClick={() => dispatch(decrement())} >DECREMENT</button>
+            <div className={classes.button}>
+                <Button variant="outlined" color="primary" onClick={() => dispatch(increment())} >INCREMENT</Button>
+                <Button variant="outlined" color="secondary" onClick={() => dispatch(decrement())} >DECREMENT</Button>
             </div>
         </div>
     );
