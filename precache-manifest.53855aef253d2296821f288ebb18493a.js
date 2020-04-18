0c50bd5b0391deeaab796db42afefab939241b9b
Yuki Yokotani <yokotani.yuki@gmail.com>
2020-04-18 15:33:06 +0900

    Updates

---
+++ precache-manifest.53855aef253d2296821f288ebb18493a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb30dd6a2c96e54a2476eab0a6b20e0",
+    "url": "/react-redux-starter-app/index.html"
+  },
+  {
+    "revision": "9c4bf11e43a86ecb5d70",
+    "url": "/react-redux-starter-app/static/js/2.fbab6491.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-redux-starter-app/static/js/2.fbab6491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d6e327daf4d595aed12",
+    "url": "/react-redux-starter-app/static/js/main.681c4325.chunk.js"
+  },
+  {
+    "revision": "02ca9aa0f57c83634cab",
+    "url": "/react-redux-starter-app/static/js/runtime-main.6493794b.js"
+  }
+]);
